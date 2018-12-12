6d82ba8ba0c147c8f32f25763ddd4ff09ad6f3b8
Reid McIlroy-Young <reidmcy@gmail.com>
2018-12-12 12:50:29 -0500

    Going to more general system

--- tf/hai-train.py
+++ tf/hai-train.py
@@ -5,6 +5,7 @@ import tempfile
 import yaml
 import argparse
 import os
+import os.path
 
 if __name__ == "__main__":
 
@@ -31,14 +32,16 @@ if __name__ == "__main__":
         with open('configs/train_prototype.yaml') as f:
             conf = yaml.safe_load(f.read())
 
-        conf['name'] = f"{os.path.basename(n.target)}-64x6"
+        name = os.path.basename(n.target[:-1]) if n.target.endswith('/') else os.path.basename(n.target)
+
+        conf['name'] = f"{name}-64x6"
         conf['gpu'] = n.gpu
         conf['dataset']['input'] = os.path.join(n.target, 'supervise-*/*.gz')
 
 
         fp.write(yaml.dump(conf))
 
-        print(f"Doing run with {n.elo} on GPU: {n.gpu}")
+        print(f"Doing run with {name} on GPU: {n.gpu}")
 
     argparser = argparse.ArgumentParser(
         description= 'Dummy args')
@@ -50,7 +53,7 @@ if __name__ == "__main__":
     argparser.add_argument('--cfg', type=argparse.FileType('r'),
         help='yaml configuration with training parameters', default = fp.name)
     argparser.add_argument('--output', type=str,
-        help='file to store weights in', default = f"full_weights/{n.elo}-64x6.txt")
+        help='file to store weights in', default = f"full_weights/{name}-64x6.txt")
 
 
     c = argparser.parse_args()
